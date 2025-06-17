209396d0ce93d1fc887615c33f0beec75b9b9e9a
Flowzym <hypophobia@gmail.com>
2025-06-17 21:54:50 +0200

    refactor: clean up React entry file

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 import 'react-quill/dist/quill.snow.css';
 
 // BOLT-UI-ANPASSUNG 2025-01-15: StrictMode entfernt um findDOMNode-Warnungen zu eliminieren
-createRoot(document.getElementById('root')!).render(
-  <App />
-);
+const container = document.getElementById('root');
+
+if (container) {
+  createRoot(container).render(<App />);
+}
