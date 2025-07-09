bf79ca6a769871142e3dcd9e934abd2f7c6d911e
Flowzym <hypophobia@gmail.com>
2025-07-09 22:35:53 +0200

    Use context state for Lebenslauf input

--- src/components/LebenslaufPreview.tsx
+++ src/components/LebenslaufPreview.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useLebenslaufData } from '../context/LebenslaufContext';
 
 export default function LebenslaufPreview() {
+  const { daten } = useLebenslaufData();
+
+  useEffect(() => {
+    console.log('LebenslaufDaten', daten);
+  }, [daten]);
+
   return (
     <div className="border border-dashed border-gray-300 rounded-lg p-4 bg-gray-50 text-gray-500 min-h-[300px]">
       <p className="italic">Hier erscheint die Vorschau deines Lebenslaufs …</p>
