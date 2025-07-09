80970fa30d1aa586b3b800b315ffc83b2945d578
Flowzym <hypophobia@gmail.com>
2025-07-09 20:04:08 +0200

    Add Lebenslauf placeholder component

---
+++ src/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+import LebenslaufPlaceholder from '@/components/LebenslaufPlaceholder.vue';
+
+// Beispiel: aktive Registerkarte ("bewerbung" oder "lebenslauf")
+const activeTab = ref('bewerbung');
+</script>
+
+<template>
+  <div>
+    <div class="mb-4 space-x-2">
+      <button @click="activeTab = 'bewerbung'" class="px-4 py-2 bg-gray-200 rounded">Bewerbung</button>
+      <button @click="activeTab = 'lebenslauf'" class="px-4 py-2 bg-gray-200 rounded">Lebenslauf</button>
+    </div>
+
+    <div v-if="activeTab === 'lebenslauf'">
+      <LebenslaufPlaceholder />
+    </div>
+  </div>
+</template>
