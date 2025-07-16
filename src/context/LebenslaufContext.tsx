b04cb821b1952a27209bda73f7003d1649c8a753
Flowzym <hypophobia@gmail.com>
2025-07-16 18:58:31 +0200

    Updated LebenslaufContext.tsx

--- src/context/LebenslaufContext.tsx
+++ src/context/LebenslaufContext.tsx
@@ -105,7 +105,6 @@ interface LebenslaufContextType {
 const LebenslaufContext = createContext<LebenslaufContextType | undefined>(undefined);
 
 export function LebenslaufProvider({
-  }
   children,
   profileSourceMappings = [],
 }: {
@@ -484,9 +483,10 @@ export function LebenslaufProvider({
     >
       {children}
     </LebenslaufContext.Provider>
+  );
+}
 
 export function useLebenslaufContext() {
-    }
   const context = useContext(LebenslaufContext);
   if (!context) {
     throw new Error('useLebenslaufContext must be used within LebenslaufProvider');
