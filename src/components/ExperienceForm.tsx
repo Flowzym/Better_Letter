37a9db41549e405c36e3c618f43dc63adf03c8a4
Flowzym <hypophobia@gmail.com>
2025-07-17 01:11:38 +0200

    Fix useMemo import error

--- src/components/ExperienceForm.tsx
+++ src/components/ExperienceForm.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import TagSelectorWithFavorites from './TagSelectorWithFavorites';
 import ZeitraumPicker from './ZeitraumPicker';
 import TasksTagInput from './TasksTagInput';
